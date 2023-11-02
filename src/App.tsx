91025488f83d75f3933b5b4ee0ac3db31f1c68b2
steveharianto <steveharianto@gmail.com>
2023-11-02 09:14:17 +0700

    Initialize react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+import Error from "./pages/Error";
 function App() {
-    return <div className="font-bold">Hello World!</div>;
+    return (
+        <Router>
+            <Routes>
+                {/* <Route path="/" element={<Home />} /> */}
+                {/* <Route path="/Login" element={<Login />} />
+                <Route path="/Register" element={<Register />} /> */}
+                <Route path="*" element={<Error />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/Error.tsx
@@ -0,0 +1,9 @@
+function Error() {
+    return (
+        <div className="w-full h-screen flex">
+            <h1 className="font-bold text-3xl text-center m-auto">PAGE NOT FOUND</h1>
+        </div>
+    );
+}
+
+export default Error;

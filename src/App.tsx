e8f0badea99ae93b17834444ef628facc3e2dab9
steveharianto <steveharianto@gmail.com>
2023-11-02 10:40:42 +0700

    Initialize Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { createContext, useState, Dispatch, SetStateAction } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+
+interface MyContextType {
+    value1: string;
+    updateValue1: Dispatch<SetStateAction<string>>;
+    value2: string;
+    updateValue2: Dispatch<SetStateAction<string>>;
+}
+
+const MyContext = createContext<MyContextType | undefined>(undefined);
+
 function App() {
+    // Context
+    const [value1, setValue1] = useState("Initial Value 1");
+    const [value2, setValue2] = useState("Initial Value 2");
+    const updateValue1: MyContextType["updateValue1"] = (newValue) => {
+        setValue1(newValue);
+    };
+    const updateValue2: MyContextType["updateValue2"] = (newValue) => {
+        setValue2(newValue);
+    };
     return (
-        <Router>
-            <Routes>
-                {/* <Route path="/" element={<Home />} /> */}
-                {/* <Route path="/Login" element={<Login />} />
+        <MyContext.Provider value={{ value1, updateValue1, value2, updateValue2 }}>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    {/* <Route path="/Login" element={<Login />} />
                 <Route path="/Register" element={<Register />} /> */}
-                <Route path="*" element={<Error />} />
-            </Routes>
-        </Router>
+                    <Route path="*" element={<Error />} />
+                </Routes>
+            </Router>
+        </MyContext.Provider>
     );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div className="bg-[#0d0221] w-full h-16 flex">
+            <div className="text-white flex items-center mx-4">
+                <p className="font-bold text-xl">StudyPal</p>
+            </div>
+        </div>
+    );
+}
+
+export default Header;

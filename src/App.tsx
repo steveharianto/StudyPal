ceaa63042f45530f9f0fb6a2b51a27c7e4e1394c
steveharianto <steveharianto@gmail.com>
2023-11-13 09:30:19 +0700

    Remove UseContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, Dispatch, SetStateAction } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Error from "./pages/Error";
@@ -11,26 +11,7 @@ import FindTutor from "./pages/FindTutor";
 import DashboardStudent from "./pages/DashboardStudent";
 import DashboardTutor from "./pages/DashboardTutor";
 
-interface MyContextType {
-    value1: string;
-    updateValue1: Dispatch<SetStateAction<string>>;
-    value2: string;
-    updateValue2: Dispatch<SetStateAction<string>>;
-}
-
-const MyContext = createContext<MyContextType | undefined>(undefined);
-
 function App() {
-    // Context
-    const [value1, setValue1] = useState("Initial Value 1");
-    const [value2, setValue2] = useState("Initial Value 2");
-    const updateValue1: MyContextType["updateValue1"] = (newValue) => {
-        setValue1(newValue);
-    };
-    const updateValue2: MyContextType["updateValue2"] = (newValue) => {
-        setValue2(newValue);
-    };
-
     // States
     const [userType, setUserType] = useState("");
 
@@ -38,29 +19,27 @@ function App() {
 
     const renderDashboard = () => {
         switch (userType) {
-          case 'Tutor':
-            return <DashboardTutor />;
-          case 'Student':
-            return <DashboardStudent />;
-          default:
-            return <Navigate to="/login" />; // Redirect to login if userType is not set
+            case "Tutor":
+                return <DashboardTutor />;
+            case "Student":
+                return <DashboardStudent />;
+            default:
+                return <Navigate to="/login" />; // Redirect to login if userType is not set
         }
-      };
+    };
     return (
-        <MyContext.Provider value={{ value1, updateValue1, value2, updateValue2 }}>
-            <Router>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register-student" element={<RegisterStudent />} />
-                    <Route path="/register-tutor" element={<RegisterTutor />} />
-                    <Route path="/find-tutor" element={<FindTutor />} />
-                    <Route path="/dashboard" element={renderDashboard()} />
-                    <Route path="*" element={<Error />} />
-                </Routes>
-            </Router>
-        </MyContext.Provider>
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register-student" element={<RegisterStudent />} />
+                <Route path="/register-tutor" element={<RegisterTutor />} />
+                <Route path="/find-tutor" element={<FindTutor />} />
+                <Route path="/dashboard" element={renderDashboard()} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+        </Router>
     );
 }
 

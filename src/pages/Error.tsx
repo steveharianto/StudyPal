123afb11d47d2fe659644484390521f771b88066
Shawn <107561331+ShawndeSheep@users.noreply.github.com>
2023-11-16 00:04:57 +0700

    404 UI

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,10 @@
+import image from '../assets/404.png'
+
 function Error() {
     return (
-        <div className="w-full h-screen flex">
-            <h1 className="font-bold text-3xl text-center m-auto">PAGE NOT FOUND</h1>
+        <div className="w-full h-screen">
+            <img src={image} className='w-1/4 ml-auto mr-auto mt-16'/>
+            <h1 className="font-bold text-3xl text-center m-auto -mt-10">PAGE NOT FOUND</h1>
         </div>
     );
 }

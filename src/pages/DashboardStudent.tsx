74ab68ec6f7a3eb91e6ebc6dddbec8b3e47df869
nicholasWijaya100 <nich.wijaya.100@gmail.com>
2023-11-22 22:00:26 +0700

    update userdashboard home ui

--- src/pages/DashboardStudent.tsx
+++ src/pages/DashboardStudent.tsx
@@ -1,5 +1,11 @@
-import React from "react";
-import { AiOutlineHome, AiOutlineMessage, AiOutlineBook, AiOutlineSetting, AiOutlineStar } from "react-icons/ai";
+import {
+  AiOutlineHome,
+  AiOutlineMessage,
+  AiOutlineBook,
+  AiOutlineSetting,
+  AiOutlineStar,
+  AiOutlineLogout,
+} from "react-icons/ai";
 import { Outlet, Link } from "react-router-dom";
 
 const DashboardStudent = () => {
@@ -9,39 +15,77 @@ const DashboardStudent = () => {
         <div className="flex items-center space-x-4">
           <img src="/logo.png" alt="Logo" className="h-10" />
           <span className="text-2xl font-bold text-blue-700">Studypal</span>
-          <Link to="/find-tutors" className="px-3 py-2 text-blue-700 hover:text-blue-800 hover:underline">Find Tutors</Link>
+          <Link
+            to="/find-tutors"
+            className="px-3 py-2 text-blue-700 hover:text-blue-800 hover:underline"
+          >
+            Find Tutors
+          </Link>
         </div>
         <div className="flex items-center space-x-6">
           <div className="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-blue-600">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-blue-600"
+            >
               {/* SVG Path */}
             </svg>
             <span className="ml-2 text-gray-700">Balance: 0 Hours</span>
           </div>
           <div className="flex items-center">
             <AiOutlineStar className="w-6 h-6 text-yellow-500" />
-            <Link to="/favorites" className="ml-2 text-gray-700 hover:text-blue-700">Favorites</Link>
+            <Link
+              to="/favorites"
+              className="ml-2 text-gray-700 hover:text-blue-700"
+            >
+              Favorites
+            </Link>
           </div>
         </div>
       </nav>
       <header className="bg-blue-700 py-4 h-[10vh]">
         <div className="mx-auto px-4">
-          <nav className="flex space-x-6 text-white text-lg">
-            <Link to="/dashboard-student/home" className="flex items-center space-x-2 hover:text-blue-300">
-              <AiOutlineHome className="text-xl" />
-              <span>Home</span>
-            </Link>
-            <Link to="/dashboard-student/messages" className="flex items-center space-x-2 hover:text-blue-300">
-              <AiOutlineMessage className="text-xl" />
-              <span>Messages</span>
-            </Link>
-            <Link to="/dashboard-student/my-lessons" className="flex items-center space-x-2 hover:text-blue-300">
-              <AiOutlineBook className="text-xl" />
-              <span>My Lessons</span>
-            </Link>
-            <Link to="/settings" className="flex items-center space-x-2 hover:text-blue-300">
-              <AiOutlineSetting className="text-xl" />
-              <span>Settings</span>
+          <nav className="flex text-white text-lg justify-between">
+            <div className="flex space-x-6">
+              <Link
+                to="/dashboard-student/home"
+                className="flex items-center space-x-2 hover:text-blue-300"
+              >
+                <AiOutlineHome className="text-xl" />
+                <span>Home</span>
+              </Link>
+              <Link
+                to="/dashboard-student/messages"
+                className="flex items-center space-x-2 hover:text-blue-300"
+              >
+                <AiOutlineMessage className="text-xl" />
+                <span>Messages</span>
+              </Link>
+              <Link
+                to="/dashboard-student/my-lessons"
+                className="flex items-center space-x-2 hover:text-blue-300"
+              >
+                <AiOutlineBook className="text-xl" />
+                <span>My Lessons</span>
+              </Link>
+              <Link
+                to="/settings"
+                className="flex items-center space-x-2 hover:text-blue-300"
+              >
+                <AiOutlineSetting className="text-xl" />
+                <span>Settings</span>
+              </Link>
+            </div>
+            <Link
+              to="/"
+              className="flex items-center space-x-2 hover:text-blue-300"
+            >
+              <AiOutlineLogout className="text-xl" />
+              <span>Logout</span>
             </Link>
           </nav>
         </div>
--- src/pages/DashboardStudentHome.tsx
+++ src/pages/DashboardStudentHome.tsx
@@ -1,55 +1,103 @@
 const DashboardStudentHome = () => {
+  const recommendedTutors = [
+    {
+      name: "Mr. Johnson",
+      subject: "Mathematics",
+      price: "$40/hour",
+      imageUrl: 'https://thefutureispublictransport.org/wp-content/uploads/2022/09/Story-C40-SiteImage_04.jpg',
+    },
+    {
+      name: "Ms. Davis",
+      subject: "Physics",
+      price: "$35/hour",
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Megawati_Sukarnoputri_official_portrait.jpg/735px-Megawati_Sukarnoputri_official_portrait.jpg',
+    },
+    {
+      name: "Dr. Thompson",
+      subject: "Chemistry",
+      price: "$45/hour",
+      imageUrl: 'https://asset.kompas.com/crops/A0qds-2BjWmU4g8xkkxJANW0Zmo=/0x0:0x0/750x500/data/photo/2022/10/24/635673c468c46.jpg',
+    },
+    // Add more tutors and lessons as needed
+  ];
+
+  const recentMessages = [
+    {
+      sender: "Instructor Smith",
+      date: "Nov 21, 2023, 10:30 AM",
+      content: "Don't forget to review the latest module on Algebra.",
+    },
+    {
+      sender: "Classmate Jane",
+      date: "Nov 20, 2023, 8:15 PM",
+      content: "Hey, are you joining the group study session tomorrow?",
+    },
+    {
+      sender: "Learning Support",
+      date: "Nov 19, 2023, 3:45 PM",
+      content:
+        "Your request for additional resources has been approved. Check your email for details.",
+    },
+    // Add more messages as needed
+  ];
+
   return (
     <>
-      <main className="max-w-6xl mx-auto px-4 py-10">
-        <section className="mb-8">
-          <h2 className="text-2xl font-bold text-gray-800 mb-6">
+      <main className="max-w-7xl mx-auto px-5 py-12 bg-gray-100">
+        <section className="mb-10 text-center">
+          <h2 className="text-3xl font-extrabold text-blue-700 mb-4">
             Welcome Back, [Student Name]!
           </h2>
-          <p className="text-lg text-gray-600">
+          <p className="text-xl text-gray-700">
             Your learning journey continues. Here's a quick overview:
           </p>
         </section>
 
-        <section className="mb-8">
-          <h3 className="text-xl font-semibold text-gray-800 mb-5">
-            Current Lessons
+        <section className="mb-10">
+          <h3 className="text-2xl font-bold text-purple-700 mb-6 text-center">
+            Recommended Tutors
           </h3>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <div className="bg-white p-6 shadow-lg rounded-lg hover:shadow-xl transition-shadow duration-200 ease-in-out">
-              <h4 className="font-semibold text-lg mb-2">Lesson Name</h4>
-              <p className="text-gray-600">Short description...</p>
-            </div>
-            {/* Repeat for other lessons */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {recommendedTutors.map((tutor, index) => (
+              <div key={index} className="bg-white p-6 shadow-md rounded-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+                <img src={tutor.imageUrl} alt={`Image of ${tutor.name}`} className="w-full h-60 object-cover rounded-lg mb-4" />
+                <h4 className="font-semibold text-lg text-green-600 mb-2">{tutor.name}</h4>
+                <p className="text-gray-700">Subject: {tutor.subject}</p>
+                <p className="text-gray-700">Price: {tutor.price}</p>
+              </div>
+            ))}
           </div>
         </section>
 
-        <section className="mb-8">
-          <h3 className="text-xl font-semibold text-gray-800 mb-5">
+        <section className="mb-10">
+          <h3 className="text-2xl font-bold text-orange-600 mb-6 text-center">
             Recent Messages
           </h3>
-          <div className="bg-white p-6 shadow-lg rounded-lg hover:shadow-xl transition-shadow duration-200 ease-in-out">
-            <p className="text-gray-600">No new messages.</p>
-          </div>
+          {recentMessages.length > 0 ? (
+            <div className="space-y-4">
+              {recentMessages.map((message, index) => (
+                <div key={index} className="bg-gradient-to-r from-blue-200 to-indigo-200 p-6 shadow-md rounded-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+                  <p className="text-gray-800 font-semibold">
+                    {message.sender} - <span className="text-gray-700">{message.date}</span>
+                  </p>
+                  <p className="text-gray-700 mt-2">{message.content}</p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="bg-white p-6 shadow-md rounded-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+              <p className="text-gray-700">No new messages.</p>
+            </div>
+          )}
         </section>
 
-        <section>
-          <h3 className="text-xl font-semibold text-gray-800 mb-5">
+        <section className="text-center">
+          <h3 className="text-2xl font-bold text-teal-700 mb-6">
             Quick Links
           </h3>
-          <div className="flex flex-wrap gap-4">
-            <a
-              href="#"
-              className="bg-blue-600 text-white px-5 py-3 rounded hover:bg-blue-700 transition-colors duration-200 ease-in-out"
-            >
-              Link 1
-            </a>
-            <a
-              href="#"
-              className="bg-blue-600 text-white px-5 py-3 rounded hover:bg-blue-700 transition-colors duration-200 ease-in-out"
-            >
-              Link 2
-            </a>
+          <div className="flex justify-center flex-wrap gap-4">
+            <a href="#" className="bg-gradient-to-r from-green-400 to-blue-500 text-white px-6 py-3 rounded-lg hover:bg-gradient-to-l transition-colors duration-300 ease-in-out">Link 1</a>
+            <a href="#" className="bg-gradient-to-r from-pink-500 to-orange-400 text-white px-6 py-3 rounded-lg hover:bg-gradient-to-l transition-colors duration-300 ease-in-out">Link 2</a>
             {/* Add more links as needed */}
           </div>
         </section>
